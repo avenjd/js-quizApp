af5c6be942df616934a45bc7b5e908a9f8c00246
avenjd <adamek.wachel@gmail.com>
2023-04-09 11:27:30 +0200

    09.05.2023

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict'
 const quizQuestionBox = document.querySelector('.quiz__question__box')
 const quizAnswers = document.querySelector('.quiz__answers')
+const submitButton = document.querySelector('.btn')
 
 const API_LINK = 'https://quizapi.io/api/v1/questions'
 const API_KEY = 'fskktED1lARHF7zv3OnMjHFuE1vMlYTY94wWbZlM'
@@ -19,37 +20,76 @@ axios
 	.get(URL)
 	.then((res) => {
 		//generowanie pytań
-		const question = document.createElement('p')
-		question.classList.add('quiz__question')
-		question.textContent = res.data[0].question
-		quizQuestionBox.append(question)
-
+		generateQuestion(res)
 		//generowanie wszystkich odpowiedzi
-		let ans = Object.values(res.data[0].answers)
-		const arrayOfAnswers = []
-		for (let i = 0; i < ans.length; i++) {
-			if (ans[i] != null) {
-				arrayOfAnswers.push(ans[i])
-			}
-		}
-		for (const e of arrayOfAnswers) {
-			const answer = document.createElement('div')
-			answer.classList.add('quiz__answer')
-			answer.textContent = e
-			quizAnswers.append(answer)
-		}
-		
-		const allAns = document.getElementsByClassName('quiz__answer')
-		console.log(allAns)
+		generateAnswers(res)
 
-		let clickEvent = () => {
-			console.log('some event content here...')
-		}
+		getAllAnswers()
 
-		Array.from(allAns).forEach((item) => {
-			item.addEventListener('click', clickEvent)
-		})
+		//funckja ktora bedzie nadawala nasluchiwanie na wszystkie answ
+		// sprwadzanie czy jakikolwiek z nich zawiera klase checked, jesli zawiera to mu ja usunac a nadac temu w ktory kliknal uzytkownik
+
+		//wcisniecie guzika podsumowanie
+		submitButton.addEventListener('click', submitAnswer)
 	})
 	.catch((e) => {
 		console.log(e)
 	})
+
+const generateQuestion = (res) => {
+	const question = document.createElement('p')
+	question.classList.add('quiz__question')
+	question.textContent = res.data[0].question
+	quizQuestionBox.append(question)
+}
+
+const generateAnswers = (res) => {
+	let ans = Object.values(res.data[0].answers)
+	const arrayOfAnswers = []
+	for (let i = 0; i < ans.length; i++) {
+		if (ans[i] != null) {
+			arrayOfAnswers.push(ans[i])
+		}
+	}
+	for (const e of arrayOfAnswers) {
+		const answer = document.createElement('div')
+		answer.classList.add('quiz__answer')
+		answer.textContent = e
+		quizAnswers.append(answer)
+	}
+}
+
+const getAllAnswers = () => {
+	//pobieranie wszystkich quizAnswerów z html kolekcji do arraya
+	const allAns = Array.from(document.getElementsByClassName('quiz__answer'))
+
+	//nadawnie nasluchiwania na wszystkie answery
+	allAns.forEach((item) => {
+		item.addEventListener('click', setOneToActive)
+	})
+}
+
+const setOneToActive = (e) => {
+	//wszystkie odpowiedzi
+	const allAns = Array.from(document.getElementsByClassName('quiz__answer'))
+
+	//wybrana odpowiedz
+	const choosedAns = e.target
+
+	//pomijanie elementu wybranego ze wszystkich
+	const allOtherAns = allAns.filter((e) => {
+		return e !== choosedAns
+	})
+
+	//ze wszystkich prócz pominiętego usuwanie klasy checked
+	allOtherAns.forEach((e) => {
+		e.classList.remove('checked')
+	})
+
+	//na wybrany element dodawanie klasy checked
+	choosedAns.classList.add('checked')
+}
+
+const submitAnswer = () => {
+	console.log('button click')
+}
